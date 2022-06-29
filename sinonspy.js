6ef2ac605ce11df9da789af95264a185186cf0cf
9kopb1337 <kingulf@mail.ru>
2022-06-29 19:01:22 +0300

    Sinon.spy decorator

---
+++ sinonspy.js
@@ -0,0 +1,10 @@
+function spy(func) {
+    function wrapper(...args) {
+        wrapper.calls.push(args);
+        return func.apply(this, argument);
+    }
+
+    wrapper.calls = [];
+
+    return wrapper;
+}
