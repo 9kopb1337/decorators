45d7791021b13823d45918d41c1917628b6a5429
9kopb1337 <kingulf@mail.ru>
2022-07-01 13:05:38 +0300

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -1,13 +1,11 @@
-function debounce(f, ms) {
-    let isCooldown = false;
-
-    return function() {
-        if (isCooldown) return;
-
-        f.apply(this, arguments);
-
-        isCooldown = true;
-
-        setTimeout(() => isCooldown = false, ms);
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
-}
+  }
+  function saveInput(){
+    console.log('Saving data');
+  }
+  const processChange = debounce(() => saveInput());
